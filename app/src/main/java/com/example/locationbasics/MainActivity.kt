f020c1e8ff38ec0f284040d2b9724c451e9eccae
RomeoV1234 <126910290+RomeoV1234@users.noreply.github.com>
2023-08-28 10:14:37 +0200

    Attempted to Show/Get co-ordinates generated

--- app/src/main/java/com/example/locationbasics/MainActivity.kt
+++ app/src/main/java/com/example/locationbasics/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.locationbasics
 
 import android.Manifest
+import android.content.ContentValues.TAG
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.util.Locale
+
 //LocationBasics - Location feature on Apps
 class MainActivity : AppCompatActivity(),LocationListener {
     private lateinit var  locationManager:LocationManager
     private lateinit var tvOuput :TextView
     private val locationPermissionCode =2
+    //Showing/Getting address
+    private lateinit var geocoder: Geocoder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +34,8 @@ class MainActivity : AppCompatActivity(),LocationListener {
         button.setOnClickListener(){
             getLocation()
         }
+        //Showing/Getting address
+        geocoder = Geocoder(this, Locale.getDefault()) //initialise geocoder
     }
 
     private fun getLocation() {
@@ -35,11 +44,18 @@ class MainActivity : AppCompatActivity(),LocationListener {
                 Manifest.permission.ACCESS_FINE_LOCATION)
                     !=PackageManager.PERMISSION_GRANTED))
         {
+            val lastLocation = fusedLocationProviderClient.lastLocation
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),locationPermissionCode)
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
             5000,5f,this)
+        //Showing/Getting address
+        Log.d(TAG, "getLastLocation: ${it.latitude}")
+        Log.d(TAG, "getLastLocation: ${it.longitude}")
+        val address = geocoder.getFromLocation(it.latitude,it.longitude, 1)
+        Log.d(TAG, "getLastLocation: ${address[0].getAddressLine(0)}")
+        Log.d(TAG, "getLastLocation: ${address[0].locality }")
     }
 
     override fun onLocationChanged(location: Location) {
